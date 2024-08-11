2c520efa13754c334b3195d8822039ea199dcdeb
Braindawgs <andrejkljajopetkovic@gmail.com>
2024-08-11 19:59:15 +0200

    Main: gracefull termination added due to signals

--- main.cpp
+++ main.cpp
@@ -5,11 +5,61 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <csignal>
+
+std::shared_ptr<msgQueue> messageQueue = nullptr;
+std::shared_ptr<ProcessA> procA = nullptr;
+std::shared_ptr<ProcessB> procB = nullptr;
+pid_t pid = -1;
+
+/**
+ * @brief Signal handler, for gracefull shutdown with Ctrl+C
+ * 
+ * @param sig 
+ */
+void signalHandler(int sig)
+{
+    std::cout << "Signal " << sig << " , Clean up invoked!" << std::endl;
+
+    if (procA)
+    {
+        std::cout << "Cleaning tasks" << std::endl;
+        procA->setTerminationFlag();
+    }
+
+    if (procB)
+    {
+        procB->setTerminationFlag();
+        procB->notifyAll();
+    }
+
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+
+    if (pid > 0) 
+    {
+        std::cout << "Cleaning fork" << std::endl;
+        kill(pid, SIGTERM);
+        int status;
+        waitpid(pid, &status, 0);
+
+        if (messageQueue)
+        {
+            messageQueue->destroyQueue();
+        }
+
+    }
+
+    exit(sig);
+}
 
 int main()
 {
-    pid_t pid = fork();
-    msgQueue queue;
+    // Register signals
+    signal(SIGTERM, signalHandler);
+    signal(SIGINT, signalHandler);
+
+    pid = fork();
+    messageQueue = std::make_shared<msgQueue>();
 
     if (0 > pid)
     {
@@ -19,13 +69,14 @@ int main()
 
     if (0 == pid)
     {
-        sleep(1); // Ensure the parent sends the message first
-        runProcessB(queue);
+        sleep(1); // Just so parent process can go first
+        procB = std::make_shared<ProcessB>();
+        procB->runProcessB(*messageQueue);
     }
     else
     {
-        runProcessA(queue);
-        wait(NULL);
+        procA = std::make_shared<ProcessA>();
+        procA->runProcessA(*messageQueue);
     }
 
     return 0;
